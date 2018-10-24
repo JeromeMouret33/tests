b1e4b928a8cc7c4de0d3577b09d7a40b0bdfc545
Mouret, Jérôme <jerome.mouret@sanofi.com>
2018-10-24 15:46:29 +0200

    Change package name base_page -> Page

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='base_page',
+setup(name='Page',
       version='1.0',
       description='Base page + Web driver object with the selenium functions',
       author='Jerome Mouret',
       author_email='jerome.mouret@sanofi.com',
       license='SANOFI',
-      packages=['base_page'],
+      packages=['Page'],
       zip_safe=False)

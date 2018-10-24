44921805047f28bf75ef8f933965decc0266d9d7
Mouret, Jérôme <jerome.mouret@sanofi.com>
2018-10-24 16:04:20 +0200

    Fix setup.py package name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='workday_page',
+setup(name='workday',
       version='1.0',
       description='Workday_page specific class for interaction with selenium base_page + locators class',
       author='Jerome Mouret',
       author_email='jerome.mouret@sanofi.com',
       license='SANOFI',
-      packages=['workday_page'],
+      packages=['workday'],
       dependency_links=['https://github.com/JeromeMouret33/tests.git@pkg/base_page'],
       zip_safe=False)

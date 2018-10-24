afa696bcfd8540977b17c0590c7373a7d7f9c902
Mouret, Jérôme <jerome.mouret@sanofi.com>
2018-10-24 16:31:47 +0200

    Add class install in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
-from setuptools import setup
+from setuptools.command.install import install as _install
+from setuptools import setup, find_packages
+import pip
 
-setup(name='workday',
+class install(_install):
+  def run(self):
+    _install.do_egg_install(self)
+
+    # just go ahead and do it
+    pip.main(['install', 'https://github.com/JeromeMouret33/tests.git@pkg/base_page'])
+
+
+
+setup(
+      name='workday',
       version='1.0',
       description='Workday_page specific class for interaction with selenium base_page + locators class',
       author='Jerome Mouret',
       author_email='jerome.mouret@sanofi.com',
       license='SANOFI',
       packages=['workday'],
-      dependency_links=['https://github.com/JeromeMouret33/tests.git@pkg/base_page'],
-      zip_safe=False)
+      cmdclass={'install': install},
+      zip_safe=False
+      )
